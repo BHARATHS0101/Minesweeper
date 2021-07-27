e975eb07f283b731fc5af5d2ecd1706b2da93e2b
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2021-07-27 18:46:01 +0530

    initial setup done

---
+++ minesweeper/src/types/reduxGlobal.d.ts
@@ -0,0 +1,16 @@
+declare namespace ReduxNS {
+    interface IState {
+      Login: LoginNS.IState;
+      Register: RegisterNS.IState;
+      ManageToken: ManageTokenNS.IState;
+      Profile: ProfileNS.IState;
+      FuelStations: FuelStationsNS.IState;
+    }
+  
+    interface IThunkFunction<Actions> {
+        (
+            dispatch: import('react').Dispatch<Actions>,
+            getAppState: () => ReduxNS.IState,
+        ): void;
+    }
+  }
